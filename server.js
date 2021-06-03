6b9466dfdb2010ad473b1d3e2f90cae242a737f0
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-06-02 23:33:53 -0500

    Added Partials in head, header, footer

--- server.js
+++ server.js
@@ -6,23 +6,26 @@ const port = 3000;
 const drinks = require(`./models/drinks`)
 const food = require(`./models/food`)
 
-
+// drinks
 app.get(`/drinks`, (req, res) => {
     res.render(`drinks_index.ejs`, {
         drinks, food
     })
 })
+
+// individual food
 app.get(`/food/:id`, (req, res) => {
     res.render(`food_show.ejs`, {
         food: food[req.params.id]
     })
 })
 
+// Individual drinks
 app.get(`/drinks/:id`, (req, res) => {
     res.render(`drinks_show.ejs`, { drinks: drinks[req.params.id] })
 })
 
-
+// listening
 app.listen(3000, () => {
     console.log(`Listening to port:${port}`)
 })
